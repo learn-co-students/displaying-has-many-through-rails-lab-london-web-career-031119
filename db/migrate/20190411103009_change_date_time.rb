9ac3c28d8020cd888b39d5a21b3c21cac84cd52f
Umar--Khan <umarkhandev@gmail.com>
2019-04-11 11:34:20 +0100

    Done.

--- db/migrate/20190411103009_change_date_time.rb
+++ db/migrate/20190411103009_change_date_time.rb
@@ -1,5 +1,5 @@
 class ChangeDateTime < ActiveRecord::Migration[5.0]
   def change
-    change_column :appointments :appointment_datetime, :datetime
+    change_column :appointments, :appointment_datetime, :datetime
   end
 end
